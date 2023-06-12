01e88c4d02b8ea47449c3ef30c311b01250b7f4c
Wajid <wajid.dev@yahoo.com>
2023-06-12 22:53:15 +0400

    selected user

--- client/components/UserAvatar.jsx
+++ client/components/UserAvatar.jsx
@@ -1,6 +1,13 @@
+import PropTypes from "prop-types";
+
 export default function UserAvatar({ userId, username }) {
+  UserAvatar.propTypes = {
+    userId: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+  };
+
   const firstLetter = username[0];
-  const intId = parseInt(userId, 16);
+  const intId = parseInt(userId.substring(10), 16);
   const bgColors = [
     "bg-cyan-500",
     "bg-blue-500",
@@ -8,6 +15,23 @@ export default function UserAvatar({ userId, username }) {
     "bg-green-500",
     "bg-lime-500",
     "bg-teal-500",
+    "bg-yellow-500",
+    "bg-indigo-500",
+    "bg-purple-500",
+    "bg-pink-500",
+    "bg-teal-500",
+    "bg-orange-500",
+    "bg-gray-500",
+    "bg-red-600",
+    "bg-blue-600",
+    "bg-green-600",
+    "bg-yellow-600",
+    "bg-indigo-600",
+    "bg-purple-600",
+    "bg-pink-600",
+    "bg-teal-600",
+    "bg-orange-600",
+    "bg-gray-600",
   ];
   const colorIndex = intId % bgColors.length;
 
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import UserAvatar from "./../../components/UserAvatar";
+import Logo from "../../components/Logo";
 
 export default function Chat() {
   const [ws, setWS] = useState(null);
   const [onlineUsers, setOnlineUsers] = useState({});
+  const [selectedUserId, setSelectedUserId] = useState(null);
 
   useEffect(() => {
     const webSocket = new WebSocket("ws://localhost:4000");
@@ -23,27 +25,55 @@ export default function Chat() {
     }
   };
 
+  const handleSelectedUser = (userId) => {
+    setSelectedUserId(userId);
+  };
+
   return (
     <HelmetProvider>
       <Helmet>
         <title>Start Your Chat</title>
       </Helmet>
       <div className="flex w-full h-screen">
-        <div className="w-1/4 bg-slate-600">
-          {Object.keys(onlineUsers).map((userId) => (
-            <div
-              key={userId}
-              className="m-5 mb-8 pb-2 cursor-pointer border-b-2 border-gray-400 text-xl text-white"
-            >
-              <div className="flex gap-2 items-center">
-                <UserAvatar userId={userId} username={onlineUsers[userId]} />
-                {onlineUsers[userId]}
+        <div className="w-1/4 bg-slate-100">
+          <a href="/">
+            <Logo />
+          </a>
+
+          {Object.keys(onlineUsers).map((userId) => {
+            const isSelectedUser = userId === selectedUserId;
+            const username = onlineUsers[userId];
+            return (
+              <div
+                key={userId}
+                className={`ml-1 my-5 cursor-pointer text-xl ${
+                  isSelectedUser && "bg-blue-200"
+                }`}
+              >
+                <div
+                  className="flex gap-x-2 items-center"
+                  onClick={() => handleSelectedUser(userId)}
+                >
+                  <div
+                    className={`h-12 pr-1 ${isSelectedUser && "bg-blue-500"}`}
+                  ></div>
+                  <UserAvatar userId={userId} username={username} />
+                  {username}
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
-        <div className="w-3/4 bg-blue-300 flex items-end">
-          <div className="flex w-full items-center mx-3 mb-2">
+        <div className="w-3/4 bg-blue-200 flex flex-col justify-between px-2">
+          <div className="text-lg py-2">
+            Send message to{" "}
+            {selectedUserId !== null ? (
+              <span className="font-bold">{onlineUsers[selectedUserId]}</span>
+            ) : (
+              "selected user"
+            )}{" "}
+          </div>
+          <div className="flex w-full items-center mb-2">
             <input
               type="text"
               className="w-full bg-white h-12 mr-2 p-2 border-none focus:outline-none"
