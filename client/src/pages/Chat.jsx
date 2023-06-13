4209865fde30f589f9691c1e91d3e611a4b7330e
Wajid <wajid.dev@yahoo.com>
2023-06-13 22:45:04 +0400

    hide chat if user is not selected

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -65,35 +65,54 @@ export default function Chat() {
           })}
         </div>
         <div className="w-3/4 bg-blue-200 flex flex-col justify-between px-2">
-          <div className="text-lg py-2">
-            Send message to{" "}
-            {selectedUserId !== null ? (
-              <span className="font-bold">{onlineUsers[selectedUserId]}</span>
-            ) : (
-              "selected user"
-            )}{" "}
-          </div>
-          <div className="flex w-full items-center mb-2">
-            <input
-              type="text"
-              className="w-full bg-white h-12 mr-2 p-2 border-none focus:outline-none"
-              placeholder="Write your message..."
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-11 h-12 cursor-pointer p-2 bg-blue-500 text-white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-              />
-            </svg>
-          </div>
+          {!selectedUserId && (
+            <div className="flex justify-center h-full items-center text-lg text-gray-600">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-7 h-7 pr-2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                />
+              </svg>
+              select user to start chat
+            </div>
+          )}
+          {selectedUserId && (
+            <>
+              <div className="text-lg py-2">
+                Send message to{" "}
+                <span className="font-bold">{onlineUsers[selectedUserId]}</span>
+              </div>
+              <div className="flex w-full items-center mb-2">
+                <input
+                  type="text"
+                  className="w-full bg-white h-12 mr-2 p-2 border-none focus:outline-none"
+                  placeholder="Write your message..."
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-11 h-12 cursor-pointer p-2 bg-blue-500 text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                  />
+                </svg>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </HelmetProvider>
